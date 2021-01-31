e814881484dc83748146ae3bdddd744a9821fde2
Thuff30 <24842812+Thuff30@users.noreply.github.com>
2021-01-31 02:30:31 -0500

    Delete message_sub.sql

--- message_sub.sql
+++
@@ -1,7 +0,0 @@
-create table `message_sub` (
-`subid` int(30) auto_increment,
-`fromuser` text not null,
-`touser` text not null,
-primary key(`subid`)
-)
-engine=myisam character set utf8 collate=utf8_general_ci;
